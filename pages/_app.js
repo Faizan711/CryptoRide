428f7cc715fc9bc0eedb7745191e2755e836b4ed
Faizan <faizanalam980@gmail.com>
2023-05-05 19:56:39 +0530

    added the favicon to the site

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import { UberProvider } from '../context/uberContext'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <UberProvider>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </UberProvider>
   )
